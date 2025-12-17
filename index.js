db08150827f82a5c165e87412115cf5fad01f320
1stntwdev <1stntw.dev@gmail.com>
2025-12-17 20:35:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi')
+// OOP
+const name= ['John', 'Jane', 'Doe'];
+const age= [25, 30, 22];
+
+const [firstName, secondName, thirdName]= name;
+console.log(secondName); // John
+
+const person = {
+  name: 'Alice',
+  age: 28,
+  city: 'New York'
+,carreer:[
+  {title: 'Engineer'},
+  {title: 'Tech Corp'}
+]};
+
+const {name:personName, age: personAge ,city:city 
+  ,carreer:[ {title:firstCarreer}, {title:secondCarreer} ]
+}= person;
+console.log(person.carreer[0].title); 
+console.log(secondCarreer)
+function showPersonInfo({brand = "unknown", model = "unknown", year = "unknown"} = {} ) {
+  console.log(`Name: ${name}, Age: ${age}, City: ${city}`);
+}
+showPersonInfo()
+
+const abc = ["a", "b", "c", "d", "e"];
+const  xyz = ["x", "y", "z"];
+const combined = [...abc, ...xyz];
+console.log(combined); // ["a", "b", "c", "d", "e", "x", "y", "z"]
